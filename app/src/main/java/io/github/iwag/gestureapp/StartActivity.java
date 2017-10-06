93245500ae988131a2c2301c2c9a3f20abf8c512
iwag <iwagame@gmail.com>
2017-10-05 18:40:49 -0700

    wipwip

--- app/src/main/java/io/github/iwag/gestureapp/StartActivity.java
+++ app/src/main/java/io/github/iwag/gestureapp/StartActivity.java
@@ -1,20 +1,32 @@
 package io.github.iwag.gestureapp;
 
 import android.content.Intent;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        mTextView = (TextView) findViewById(R.id.textView2);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        // TODO
+        return true;
     }
 
     public void doStart(View v) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+//        Intent i = new Intent(this, MainActivity.class);
+//        startActivity(i);
     }
 }
